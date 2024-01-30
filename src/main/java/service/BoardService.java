ad101d6f0c0f32451d4df33182059f1184d6a10d
dongh810 <105986200+dongh810@users.noreply.github.com>
2024-01-31 00:03:38 +0900

    initial commit

---
+++ src/main/java/service/BoardService.java
@@ -0,0 +1,37 @@
+package service;
+
+import aggregate.Board;
+import repository.BoardRepository;
+
+import java.lang.reflect.Member;
+import java.util.ArrayList;
+
+public class BoardService {
+
+    private final BoardRepository br = new BoardRepository();
+
+    public BoardService() {
+    }
+
+    public void selectAllBoards() {
+        ArrayList<Board> selectedBoards = br.selectAllBoard();
+
+        /* 설명. 회원이 한명도 없어서 조회 결과가 없더라도 ArrayList객체는 넘어온다.(Empty상태로) */
+        if(!selectedBoards.isEmpty()) {        // 회원이 한명이라도 조회 된다면
+            System.out.println("==== service까지 잘 반환되어 오나 확인 ====");
+            for(Board b: selectedBoards) {
+                System.out.println(b);
+            }
+
+            return;                             // 이후 코드와 상관 없이 메소드 종료
+        }
+
+        /* 설명. 조건이 맞지 않아(회원이 조회되지 않아) 출력을 하는 구문(위의 조건이 맞으면 실행되지 않음)(feat.else 안 쓰기) */
+        System.out.println("슬프게도 우리 게시판은 아직 글이 없습니다.");
+    }
+
+    public void searchContent() {
+        br.searchContent();
+        System.out.println("내용검색 기능!");
+    }
+}
